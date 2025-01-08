1291bb1f6074c40606d3990f291e87ef25e64d91
oswaldojmp <oswaldojesusmarinpages@gmail.com>
2025-01-08 13:03:57 -0400

    Agregar Archivos al Repositorio
    Agregando archivos al repositorio local Git

---
+++ Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InsertDateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SortTextToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GuardarArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.txtNota = New System.Windows.Forms.TextBox()
+        Me.etiNota = New System.Windows.Forms.Label()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem, Me.InsertDateToolStripMenuItem, Me.SortTextToolStripMenuItem, Me.GuardarArchivoToolStripMenuItem, Me.CloseToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.ArchivoToolStripMenuItem.Text = "&Archivo"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.OpenToolStripMenuItem.Text = "A&brir"
+        '
+        'InsertDateToolStripMenuItem
+        '
+        Me.InsertDateToolStripMenuItem.Name = "InsertDateToolStripMenuItem"
+        Me.InsertDateToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.InsertDateToolStripMenuItem.Text = "&Agregar Fecha"
+        '
+        'SortTextToolStripMenuItem
+        '
+        Me.SortTextToolStripMenuItem.Name = "SortTextToolStripMenuItem"
+        Me.SortTextToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SortTextToolStripMenuItem.Text = "&Ordenar Texto"
+        '
+        'GuardarArchivoToolStripMenuItem
+        '
+        Me.GuardarArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveAsToolStripMenuItem})
+        Me.GuardarArchivoToolStripMenuItem.Name = "GuardarArchivoToolStripMenuItem"
+        Me.GuardarArchivoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.GuardarArchivoToolStripMenuItem.Text = "G&uardar Archivo"
+        '
+        'SaveAsToolStripMenuItem
+        '
+        Me.SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        Me.SaveAsToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SaveAsToolStripMenuItem.Text = "&Guardar Como"
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CloseToolStripMenuItem.Text = "&Cerrar"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "&Salir"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'txtNota
+        '
+        Me.txtNota.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtNota.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtNota.Font = New System.Drawing.Font("Ebrima", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNota.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.txtNota.Location = New System.Drawing.Point(32, 49)
+        Me.txtNota.Multiline = True
+        Me.txtNota.Name = "txtNota"
+        Me.txtNota.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtNota.Size = New System.Drawing.Size(741, 377)
+        Me.txtNota.TabIndex = 5
+        '
+        'etiNota
+        '
+        Me.etiNota.AutoSize = True
+        Me.etiNota.Font = New System.Drawing.Font("Ebrima", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.etiNota.Location = New System.Drawing.Point(28, 25)
+        Me.etiNota.Name = "etiNota"
+        Me.etiNota.Size = New System.Drawing.Size(391, 21)
+        Me.etiNota.TabIndex = 4
+        Me.etiNota.Text = "Cargue un Archivo de Texto con el Comando Abrir"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtNota)
+        Me.Controls.Add(Me.etiNota)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Ordenar Texto"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents txtNota As TextBox
+    Friend WithEvents etiNota As Label
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents GuardarArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InsertDateToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SortTextToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub ShellSort(ByRef ordenar() As String, ByVal numDeElementos As Short)
+        Dim temp As String
+        Dim i, j, abarcar As Short
+
+        'El Procedimiento ShellSort organiza los elementos de ordenar()
+        'en orden descendente y los devuelve al procedimiento que los invoco
+
+        abarcar = numDeElementos / 2
+        Do While abarcar > 0
+            For i = abarcar To numDeElementos - 1
+                For j = (i - abarcar + 1) To 1 Step -abarcar
+                    If ordenar(j) <= ordenar(j + abarcar) Then Exit For
+
+                    'Revisa los elementos del Arreglo que estan en desorden
+                    temp = ordenar(j)
+                    ordenar(j) = ordenar(j + abarcar)
+                    ordenar(j + abarcar) = temp
+                Next j
+            Next i
+            abarcar = abarcar / 2
+        Loop
+    End Sub
+
+End Module
